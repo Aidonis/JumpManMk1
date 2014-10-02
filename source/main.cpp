ca150d4633b33ca628bc32f26b1d5156fca16bf2
Aidan Nabass <ai.nabass@gmail.com>
2014-10-02 12:09:47 -0700

    Start Jump Man
    Empty Template for Jump Man

---
+++ source/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "AIE.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	Initialise(800, 600, false, "JumpMan");
+
+	SetBackgroundColour(SColour(0, 0, 0, 255));
+
+
+	//Game Loop
+	do
+	{
+		ClearScreen();
+
+	} while (!FrameworkUpdate());
+
+	Shutdown();
+
+	return 0;
+}
